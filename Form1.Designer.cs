eded3d89d40245f8b672588205e178e9eb04bb06
Nick Fu <hfu36.gatech@gmail.com>
2018-03-12 02:41:10 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ClientTestVision2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SendInfo = new System.Windows.Forms.TextBox();
+            this.recInfo = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.ReSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 44);
+            this.textBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SendInfo
+            // 
+            this.SendInfo.Location = new System.Drawing.Point(1, 90);
+            this.SendInfo.Multiline = true;
+            this.SendInfo.Name = "SendInfo";
+            this.SendInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.SendInfo.Size = new System.Drawing.Size(486, 113);
+            this.SendInfo.TabIndex = 2;
+            // 
+            // recInfo
+            // 
+            this.recInfo.Location = new System.Drawing.Point(1, 246);
+            this.recInfo.Multiline = true;
+            this.recInfo.Name = "recInfo";
+            this.recInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.recInfo.Size = new System.Drawing.Size(486, 113);
+            this.recInfo.TabIndex = 3;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(322, 209);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(75, 23);
+            this.Send.TabIndex = 4;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // ReSend
+            // 
+            this.ReSend.Location = new System.Drawing.Point(412, 209);
+            this.ReSend.Name = "ReSend";
+            this.ReSend.Size = new System.Drawing.Size(75, 23);
+            this.ReSend.TabIndex = 5;
+            this.ReSend.Text = "ReSend";
+            this.ReSend.UseVisualStyleBackColor = true;
+            this.ReSend.Click += new System.EventHandler(this.ReSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 380);
+            this.Controls.Add(this.ReSend);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.recInfo);
+            this.Controls.Add(this.SendInfo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox SendInfo;
+        private System.Windows.Forms.TextBox recInfo;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button ReSend;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace ClientTestVision2
+{
+    public partial class Form1 : Form
+    {
+        //string strSend = "";
+        string server = "10.1.12.224";
+        string message = "You're so funny!";
+        NetworkStream stream;
+        Byte[] data;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.textBox1.Text = DateTime.Now.ToString();
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            Connect(server, message);
+        }
+
+        private void ReSend_Click(object sender, EventArgs e)
+        {
+            WriteInfo(stream, data);
+            ReadInfo(stream, data);
+        }
+
+        private void WriteInfo(NetworkStream stream, Byte[] data)
+        { 
+            stream.Write(data, 0, data.Length);
+            SendInfo.AppendText(DateTime.Now.ToString() + "\r\n");
+            SendInfo.AppendText("Sent:" + message + "\r\n");
+        }
+
+        private void ReadInfo(NetworkStream stream, Byte[] data)
+        {
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+            recInfo.AppendText(DateTime.Now.ToString() + "\r\n");
+            recInfo.AppendText("Received: " + responseData + "\r\n");
+            // Close everything.
+        }
+
+        private void Connect(String server, String message)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 8000;
+                TcpClient client = new TcpClient(server, port);
+                // Translate the passed message into ASCII and store it as a Byte array.
+                this.data = System.Text.Encoding.ASCII.GetBytes(message);
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+                this.stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                WriteInfo(stream, data);
+                // Receive the TcpServer.response.
+                ReadInfo(stream, data);
+
+                // Close everything.
+                //stream.Close();
+                //client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                MessageBox.Show("ArgumentNullException: {0}", e.ToString());
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show("SocketException: {0}", e.ToString());
+            }
+
+            //Console.WriteLine("\n Press Enter to continue...");
+            //Console.Read();
+        }
+
+    }
+}
